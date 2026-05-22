df6a391199808b1ccbea85df22c07fcba7fcee6a
helloskmodel <happysunkai2018@gmail.com>
2026-05-22 16:37:58 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,3 @@
-import "./styles/global.css";
-import Header from "./components/Header.jsx";
-import Home from "./pages/Home.jsx";
-
 export default function App() {
-  return (
-    <div className="site-shell">
-      <Header activePage="home" />
-      <Home />
-    </div>
-  );
+  return <h1>UVMARK TEST PAGE</h1>;
 }
